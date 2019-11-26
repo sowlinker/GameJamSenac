e1a931b86a4fab60764985fd2aad53cbdc25afd3
Sowlinker Castro <Darkbts3@hotmail.com>
2019-11-26 16:44:26 -0300

    Add project files.

---
+++ AlchemistJam/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using Otter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlchemistJam
+{
+    class GameManager
+    {
+               
+        public void StartGame(Scene scene)
+        {
+            var mainGame = new Game("Chemical Mage",860,640);
+            mainGame.Start(scene);
+        }
+       
+       
+    }
+
+    class Laboratory : Entity
+    {
+        public Laboratory(float x, float y, string imagePath) : base(x, y)
+        {
+            //Create an image using the path passed in with constructor
+            var image = new Image(imagePath);
+            //Add the image  to the Entity's Graphic list. 
+            image.CenterOrigin();
+            AddGraphic(image);
+        }
+    }
+}
---
+++ AlchemistJam/Program.cs
@@ -0,0 +1,23 @@
+﻿using Otter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlchemistJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var play = new GameManager();
+            var scene = new Scene();
+            scene.Add(new Laboratory(500, 500, @"C:\Vs\GameJamSenac\AlchemistJam\Asset\chemical-laboratory.png"));
+
+            play.StartGame(scene);
+            
+
+        }
+    }
+}
